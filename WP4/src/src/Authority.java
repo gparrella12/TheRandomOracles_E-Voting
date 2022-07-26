030fe1105dcfd84cbdcdcdef3972f586ec7eac9f
gparrella12 <g.parrella9@studenti.unisa.it>
2022-07-26 19:44:12 +0200

    Added some part of Authority code

--- WP4/src/src/Authority.java
+++ WP4/src/src/Authority.java
@@ -1,21 +1,75 @@
 package src;
 
 import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Authority {
 
     private String name;
 
-    private Key privateEncKey;
+    private final Key privateEncKey;
 
-    private Key publicEncKey;
+    private final Key publicEncKey;
 
-    private Key privateSigKey;
+    private final Key privateSigKey;
 
-    private Key publicSigKey;
+    private final Key publicSigKey;
 
     private Object authorityCertificate;
 
-    public Authority() {
+    public Authority(String name) {
+        this.name = name;
+        
+        SecureRandom random = new SecureRandom();
+
+        KeyPairGenerator generatorEnc = null;
+        KeyPairGenerator generatorSig = null;
+        try {
+            generatorEnc = KeyPairGenerator.getInstance("ElGamal"); // generatorEnc of the keys
+            generatorSig = KeyPairGenerator.getInstance("ECDSA"); // generatorSig of the keys
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Authority.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(ex.getMessage());
+            System.exit(-1);
+        }
+        // VERIFICARE IL PARAMETRO DA INSERIRE SUCCESSIVAMENTE
+        generatorEnc.initialize(2048, random); // initialize with 2048 bits of security with a secure random source
+
+        KeyPair pair = generatorEnc.generateKeyPair(); // generate a pair of PK/SK 
+        this.publicEncKey = pair.getPublic(); // method getPublic returns PK
+        this.privateEncKey = pair.getPrivate(); // getPrivate SK
+  
+        pair = generatorSig.generateKeyPair(); // generate a pair of PK/SK 
+        this.publicSigKey = pair.getPublic(); // method getPublic returns PK
+        this.privateSigKey = pair.getPrivate(); // getPrivate SK       
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public Key getPublicEncKey() {
+        return publicEncKey;
+    }
+
+    public Key getPublicSigKey() {
+        return publicSigKey;
+    }
+
+    public Object getAuthorityCertificate() {
+        return authorityCertificate;
+    }
+
+    @Override
+    public String toString() {
+        return "Authority{" + "name=" + name + ", publicEncKey=" + publicEncKey + ", publicSigKey=" + publicSigKey + '}';
+    }
+
+    
+    
 }
---
+++ WP4/src/src/Utils.java
@@ -0,0 +1,30 @@
+package src;
+
+/**
+ *
+ * @author gparrella
+ */
+public class Utils {
+
+    private static String digits = "0123456789abcdef";
+
+    public static String toString(byte[] bytes) {
+        char[] chars = new char[bytes.length];
+
+        for (int i = 0; i != chars.length; i++) {
+            chars[i] = (char) (bytes[i] & 0xff);
+        }
+
+        return new String(chars);
+    }
+
+    public static String toHex(byte[] data) {
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i != data.length; i++) {
+            int v = data[i] & 0xff;
+            buf.append(digits.charAt(v >> 4));
+            buf.append(digits.charAt(v & 0xf));
+        }
+        return buf.toString();
+    }
+}
