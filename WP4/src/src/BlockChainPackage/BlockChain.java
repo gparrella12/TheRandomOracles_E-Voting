e49d2c0dd8561e353dd5b94a1e6e7b922005fd5a
Speranza Ranieri <s.ranieri4@studenti.unisa.it>
2022-07-28 00:42:04 +0200

    Read from file and write on file implementation added

--- WP4/src/src/BlockChainPackage/BlockChain.java
+++ WP4/src/src/BlockChainPackage/BlockChain.java
@@ -1,7 +1,12 @@
 package src.BlockChainPackage;
 
+import java.io.*;
 import java.util.*;
 
+/**
+ *
+ * @author Speranza
+ */
 public class BlockChain {
 
     private List<Block> chain;
@@ -9,14 +14,45 @@ public class BlockChain {
     public BlockChain() {
         chain = new ArrayList<>();
     }
-    // ho aggiunto questo costruttore per quando la blockchain la dobbiamo richiamare
-    // da file di testo, dopo la prima volta
-    public BlockChain(String filename) {
-        
+
+    // costruttore da utilizzare quando bisogna richiamare la blockchain
+    // da file di testo, dopo la prima inizializzazione.
+    public BlockChain(String fileName) {
+        chain = readFromFile(fileName);
     }
 
-    void addBlock(Block b) {
-        chain.add(b);
+    void addBlock(String fileName, Block block) {
+        chain.add(block);
+        writeOnFile(fileName, block);
     }
-    
+
+    private static void writeOnFile(String fileName, Block block) {
+
+        try ( PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)))) {
+            pw.print(block.toString());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+    private static List<Block> readFromFile(String fileName) {
+
+        List<Block> temporaryChain = new ArrayList<>();
+        
+        try ( ObjectInputStream din = new ObjectInputStream(new BufferedInputStream(new FileInputStream(fileName)))) {
+
+            Block b = (Block) din.readObject();
+            temporaryChain.add(b);
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found.");
+            return null;
+        } catch (IOException | ClassNotFoundException ex) {
+            System.out.println("Error during opening file.");
+            return null;
+        }
+        return temporaryChain;
+    }
+
 }
