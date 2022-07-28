010d7310ae19715b8cd10a5387489d0ec2da2fb6
gparrella12 <g.parrella9@studenti.unisa.it>
2022-07-28 10:33:24 +0200

    Add certificates for all the authorities, management of key pair of signatures for authorities

--- WP4/src/src/AuthorityPackage/Authority.java
+++ WP4/src/src/AuthorityPackage/Authority.java
@@ -1,14 +1,19 @@
 package src.AuthorityPackage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import src.ElGamalHomomorphic.ElGamalKeyPair;
 import java.math.BigInteger;
-import java.security.Key;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
 
 /**
  *
@@ -19,9 +24,9 @@ public class Authority {
     private String name;
     private final BigInteger privateEncKey;
     private final BigInteger publicEncKey;
-    private final Key privateSigKey;
-    private final Key publicSigKey;
-    private Object certificate;
+    private final PrivateKey privateSigKey;
+    private final PublicKey publicSigKey;
+    private X509Certificate certificate;
 
     //nel costruttore creo la coppia (sk,pk);   
     public Authority(String name) {
@@ -30,23 +35,49 @@ public class Authority {
         this.privateEncKey = encPair.getSecretKey();
         this.publicEncKey = encPair.getPublicKey();
 
-        SecureRandom random = new SecureRandom();
-        KeyPairGenerator generator = null;
+        // Read the authority Certificate
+        InputStream in = null;
         try {
-            generator = KeyPairGenerator.getInstance("ECDSA"); // generator of the keys
-        } catch (NoSuchAlgorithmException ex) {
+            in = new FileInputStream("Certificati/" + name + ".crt");
+        } catch (FileNotFoundException ex) {
             ex.printStackTrace();
-            System.exit(-1);
         }
-        // with a secure random source
-        KeyPair sigPair = generator.generateKeyPair(); // generate a encPair of PK/SK 
-        this.publicSigKey = sigPair.getPublic(); // method getPublic returns PK
-        this.privateSigKey = sigPair.getPrivate(); // getPrivate SK
+        // create the certificate factory 
+        CertificateFactory fact = null;
+        try {
+            fact = CertificateFactory.getInstance("X.509");
+        } catch (CertificateException ex) {
+            ex.printStackTrace();
+        }
 
-        this.certificate = null;
+        try {
+            // read the certificate
+            this.certificate = (X509Certificate) fact.generateCertificate(in);
+        } catch (CertificateException ex) {
+            ex.printStackTrace();
+        }
+        this.publicSigKey = this.certificate.getPublicKey();
+        PrivateKey key = null;
+        try {
+            key = get("Certificati/" + name + ".p8");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        this.privateSigKey = key;
     }
 
-    public Key getPrivateSigKey() {
+    public static PrivateKey get(String filename)
+            throws Exception {
+
+        byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
+
+        PKCS8EncodedKeySpec spec
+                = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("ECDSA");
+        return kf.generatePrivate(spec);
+    }
+
+    public PrivateKey getPrivateSigKey() {
         return privateSigKey;
     }
 
@@ -58,12 +89,15 @@ public class Authority {
         return privateEncKey;
     }
 
-    public Object getPublicSigKey() {
+    public PublicKey getPublicSigKey() {
         return publicSigKey;
     }
 
-    public Object getCertificate() {
+    public X509Certificate getCertificate() {
         return certificate;
     }
 
+    public String getName() {
+        return name;
+    }
 }
