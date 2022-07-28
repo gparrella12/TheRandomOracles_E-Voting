17a8d4d64c9ccfe2e81b750451ab2f69e042514a
Speranza Ranieri <s.ranieri4@studenti.unisa.it>
2022-07-28 23:03:39 +0200

    Package name changed

---
+++ WP4/src/src/VoterPackage/Loader.java
@@ -0,0 +1,68 @@
+package src.VoterPackage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+/**
+ *
+ * @author fsonnessa
+ */
+public class Loader {
+
+    private static X509Certificate lastCrt = null;
+
+    public static X509Certificate loadCrtFromFile(String filename) {
+        // Read the authority Certificate
+        InputStream in = null;
+        try {
+            in = new FileInputStream(filename);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        // create the certificate factory 
+        CertificateFactory fact = null;
+        try {
+            fact = CertificateFactory.getInstance("X.509");
+        } catch (CertificateException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            // read the certificate
+            lastCrt = (X509Certificate) fact.generateCertificate(in);
+        } catch (CertificateException ex) {
+            ex.printStackTrace();
+        }
+
+        return lastCrt;
+    }
+
+    public static X509Certificate getLastCrt() {
+        return lastCrt;
+    }
+
+    public static PrivateKey loadSkFromFile(String filename) {
+        PrivateKey key = null;
+        try {
+            byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("ECDSA", new BouncyCastleProvider());
+            key = kf.generatePrivate(spec);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        return key;
+    }
+
+}
