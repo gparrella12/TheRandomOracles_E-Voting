53312dde45a697d9531b258d75eb38ac412b0d1f
Speranza Ranieri <s.ranieri4@studenti.unisa.it>
2022-07-28 23:01:17 +0200

    Vote toString added

---
+++ WP4/src/src/VoterPackage/Vote.java
@@ -0,0 +1,36 @@
+package src.VoterPackage;
+
+import java.security.cert.X509Certificate;
+import src.ElGamalHomomorphic.ElGamalCipherText;
+
+public class Vote {
+
+    private ElGamalCipherText encVote;
+    private X509Certificate voterCertificate;
+    private boolean voteProof;
+
+    public Vote(ElGamalCipherText encVote, X509Certificate voterCertificate, boolean voteProof) {
+        this.encVote = encVote;
+        this.voterCertificate = voterCertificate;
+        this.voteProof = voteProof;
+    }
+
+    public ElGamalCipherText getEncVote() {
+        return encVote;
+    }
+
+    public X509Certificate getVoterCertificate() {
+        return voterCertificate;
+    }
+
+    public boolean isVoteProof() {
+        return voteProof;
+    }
+
+    @Override
+    public String toString() {
+        return "\nEncVote=" + encVote + "\nVoterCertificate=" + voterCertificate + "\nVoteProof=" + voteProof;
+    }
+
+    
+}
