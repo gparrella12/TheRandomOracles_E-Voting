3db23b277b473578d5a7b7e3ddea74bda44f278d
Speranza Ranieri <s.ranieri4@studenti.unisa.it>
2022-07-30 22:20:00 +0200

    AuthorityBlock javadoc added

--- WP4/src/src/BlockChainPackage/AuthorityBlock.java
+++ WP4/src/src/BlockChainPackage/AuthorityBlock.java
@@ -7,11 +7,13 @@ import java.security.cert.X509Certificate;
 import src.SchnorrNIZKP.SchnorrNIProof;
 
 /**
+ * This class represents a block containing the information about an Authority;
+ * there will be an Authority Block for each Authority.
  *
  * @author fsonnessa
  */
-public class AuthorityBlock implements Serializable{
-    
+public class AuthorityBlock implements Serializable {
+
     private String name;
     private BigInteger publicEncKey;
     private PublicKey publicSigKey;
@@ -19,12 +21,15 @@ public class AuthorityBlock implements Serializable{
     private SchnorrNIProof proof;
 
     /**
+     * Constructor of <code>AuthorityBlock</code> class.
      *
-     * @param name
-     * @param publicEncKey
-     * @param publicSigKey
-     * @param certificate
-     * @param proof
+     * @param name name associated to the Authority.
+     * @param publicEncKey public Encryption Key.
+     * @param publicSigKey public Signature Key.
+     * @param certificate certificate (N.B. for certificate we mean the lined
+     * list of certificates down to the root (that is, the certification
+     * authority).
+     * @param proof proof of correctness of the vote.
      */
     public AuthorityBlock(String name, BigInteger publicEncKey, PublicKey publicSigKey, X509Certificate certificate, SchnorrNIProof proof) {
         this.name = name;
@@ -35,30 +40,37 @@ public class AuthorityBlock implements Serializable{
     }
 
     /**
+     * Function used to get the public Encyprion key.
      *
-     * @return
+     * @return a <code>BigInteger</code> representing the public Encryption Key.
      */
     public BigInteger getPublicEncKey() {
         return publicEncKey;
     }
 
     /**
+     * Function used to get the public Signature key.
      *
-     * @return
+     * @return a <code>PublicKey</code> object representing the public Signature
+     * Key.
      */
     public PublicKey getPublicSigKey() {
         return publicSigKey;
     }
 
     /**
+     * Function used to get the certificate.
      *
-     * @return
+     * @return a <code>X509Certificate</code> object representing the
+     * certificate.
      */
     public X509Certificate getCertificate() {
         return certificate;
     }
 
     /**
+     * Function used to print the informations on the BlockChain (in our case,
+     * on a file .txt)
      *
      * @return
      */
