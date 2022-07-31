ee3275534319e2725168688f590bcb937666347e
Speranza Ranieri <s.ranieri4@studenti.unisa.it>
2022-07-31 18:27:30 +0200

    javadoc modified

--- WP4/src/src/BlockChainPackage/VoteTransaction.java
+++ WP4/src/src/BlockChainPackage/VoteTransaction.java
@@ -8,7 +8,7 @@ import src.Utils.Utils;
 import src.VoterPackage.VoteProof;
 
 /**
- * This class represents a <code>Vote</code> transaction.
+ * This Class represents a <code>Vote</code> transaction.
  *
  * @author Speranza
  */
@@ -20,19 +20,19 @@ public class VoteTransaction implements Serializable {
     private Timestamp timestamp;
 
     /**
-     * 
+     *
      * Creates the <code>VoteTransaction</code>, by passing:
      *
      * <ul>
      * <li><code> the vote of a voter</code></li>
-     * <li><code> the proof of the validity of the vote</code></li>
-     * <li><code> the signature of (vote || vote's proof) </code></li>
-     * </ul>  
+     * <li><code> the proof of validity of the vote</code></li>
+     * <li><code> the signature of (vote || proof of vote) </code></li>
+     * </ul>
      * Constructor of the <code>VoteTransaction</code> Class.
      *
      * @param vote vote made by a voter
      * @param vp proof of validity of the vote
-     * @param sign signature of (vote || vote's proof)
+     * @param sign signature of (vote || proof of vote)
      */
     public VoteTransaction(Vote vote, VoteProof vp, byte[] sign) {
         this.vote = vote;
@@ -42,9 +42,9 @@ public class VoteTransaction implements Serializable {
     }
 
     /**
-     * This method prints information of the vote transaction
+     * This method prints information of the vote transaction.
      *
-     * @return a string containing information of the vote transaction
+     * @return a <code>String</code> containing information of the vote transaction.
      */
     @Override
     public String toString() {
