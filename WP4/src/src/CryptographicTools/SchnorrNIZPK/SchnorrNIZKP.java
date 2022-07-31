51db4a644631e87fae8fbb9e68c28f771e443e1f
Speranza Ranieri <s.ranieri4@studenti.unisa.it>
2022-07-31 18:39:32 +0200

    code formatted

--- WP4/src/src/CryptographicTools/SchnorrNIZPK/SchnorrNIZKP.java
+++ WP4/src/src/CryptographicTools/SchnorrNIZPK/SchnorrNIZKP.java
@@ -6,29 +6,28 @@ import src.CryptographicTools.CryptographicHash;
 import src.CryptographicTools.ElGamalHomomorphic.CyclicGroupParameters;
 
 /**
- * This class contains some static methods to implements the Schnorr NIZKP.
+ * This Class contains some static methods to implements the Schnorr NIZKP.
  *
  * @author gparrella
  */
 public class SchnorrNIZKP {
-      
+
     /**
-     * This method allows to make the Schnorr Non-interactive Zero-Knowledge Proof     
-     * 
-     *  The claim of the proof is the following:
-     *  d<sub>A<sub>i</sub>, voti <sub>&Omega;<sub>1</sub></sub></sub>
-     *  = (g<sup>r</sup>)<sup>x<sub>A<sub>i</sub></sub></sup>
-     *  &and; 
-     *  PK<sub>A<sub>i</sub></sub> 
-     *  = 
-     *  g<sup>x<sub>A<sub>i</sub></sub></sup>
+     * This method allows to make the Schnorr Non-interactive Zero-Knowledge
+     * Proof.
+     *
+     * The claim of the proof is the following: d<sub>A<sub>i</sub>, voti
+     * <sub>&Omega;<sub>1</sub></sub></sub>
+     * = (g<sup>r</sup>)<sup>x<sub>A<sub>i</sub></sub></sup>
+     * &and; PK<sub>A<sub>i</sub></sub>
+     * =
+     * g<sup>x<sub>A<sub>i</sub></sub></sup>
      *
      * @param x the secrete key associated to y
-     * @param y the public key, y=g^x 
+     * @param y the public key, y=g^x
      * @param param the parameters of a cyclic group of order q
      * @return a <code> SchnorrNIProof </code> object
      */
-    
     public static SchnorrNIProof makeProof(BigInteger x, BigInteger y, CyclicGroupParameters param) {
         int securityParameter = param.getSecurityParameter().intValue();
         BigInteger q = param.getQ();
@@ -51,10 +50,11 @@ public class SchnorrNIZKP {
     }
 
     /**
-     * This method checks if a Schnorr NIZKP is valid with respect to a 
-     * public key y
+     * This method checks if a Schnorr NIZKP is valid with respect to a public
+     * key y.
      *
-     * @param proof the proof, represented by a <code>SchnorrNIProof</code> object
+     * @param proof the proof, represented by a <code>SchnorrNIProof</code>
+     * object
      * @param y the public key, y=g^x
      * @param param the parameters of a cyclic group of order q
      * @return true if the NIZKP is valid, false otherwise
@@ -65,25 +65,25 @@ public class SchnorrNIZKP {
         BigInteger p = param.getP();
         BigInteger q = param.getQ();
         // Get the value of the NIZKP
-        
+
         BigInteger a = proof.getA();
         BigInteger c = proof.getC();
         BigInteger z = proof.getZ();
-        
+
         // Compute the digest of y || a
         BigInteger toHash = new BigInteger(y.toString().concat(a.toString()));
-        
+
         // c1 = H(y || a), con y=g^x mod p
-        BigInteger c1 = new BigInteger(CryptographicHash.hash(toHash.toByteArray())); 
+        BigInteger c1 = new BigInteger(CryptographicHash.hash(toHash.toByteArray()));
 
         // Verify if c value is equal to H(y || a) previously computed
         if (c.equals(c1)) {
             // compute k = g^z mod p
             BigInteger k = g.modPow(z.mod(q), p);
-            
+
             // compute a * y^c mod p
             BigInteger res = a.multiply(y.modPow(c.mod(q), p)).mod(p);
-            
+
             // verify if a*y^c = g^z
             return k.equals(res);
         }
